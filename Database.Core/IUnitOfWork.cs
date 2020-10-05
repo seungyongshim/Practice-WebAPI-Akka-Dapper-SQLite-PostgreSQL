510dacd7c86eb1822be7b001f3c2f1d0e154d886
심승용 <seungyong.shim@mirero.co.kr>
2020-10-05 23:57:12 +0900

    IUnitOfWork 추가

---
+++ Database.Core/IUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace Database.Core
+{
+    internal interface IUnitOfWork<T> : IDisposable
+    {
+    }
+}
--- WebApp/Actors/GetUserActor.cs
+++ WebApp/Actors/GetUserActor.cs
@@ -1,6 +1,9 @@
 ﻿using Akka.Actor;
 using Akka.Event;
 using Database.Core;
+using Domain;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Linq;
 using WebApp.Messages;
 
@@ -9,18 +12,24 @@ namespace WebApp.Actors
     public class GetUserActor : ReceiveActor
     {
         public ILoggingAdapter Logger { get; } = Context.GetLogger();
-        public GetUserActor(IUserRepository userRepository)
+        public GetUserActor(IServiceProvider serviceProvider, IUserRepository userRepository)
         {
+            ServiceProvider = serviceProvider;
             UserRepository = userRepository;
             Receive<GetUserMessage>(Handle);
         }
 
         private void Handle(GetUserMessage msg)
         {
-            Logger.Debug("GetUserMessage Start");
-            Sender.Tell(UserRepository.FindAll().ToList());
+            using (var s = ServiceProvider.CreateScope())
+            {
+                s.ServiceProvider.GetService<IUnitOfWork<User>>();
+                Sender.Tell(UserRepository.FindAll().ToList());
+            }
+                
         }
 
+        public IServiceProvider ServiceProvider { get; }
         public IUserRepository UserRepository { get; }
     }
 }
